402588893c10b7720b6784ebb92ef19cd5f74591
Jascha030 <contact@jaschavanaalst.nl>
2022-06-22 00:01:54 +0200

    ✨ Interface for Config repositories based around a config file.

--- src/Config/Repository/ConfigFileRepositoryInterface.php
+++ src/Config/Repository/ConfigFileRepositoryInterface.php
@@ -1,8 +1,29 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Jascha030\Dotfiles\Config\Repository;
 
-interface ConfigFileRepositoryInterface
-{
+use Jascha030\Dotfiles\Config\Parser\ConfigFileParserInterface;
 
+interface ConfigFileRepositoryInterface extends ConfigRepositoryInterface
+{
+    /**
+     * A (or multiple) pattern(s) to find a valid config file.
+     *
+     * @return string|array
+     */
+    public function getPatterns(): string|array;
+
+    /**
+     * @return ConfigFileParserInterface[]
+     */
+    public function getAvailableParsers(): array;
+
+    /**
+     * Path to the default config of this format.
+     *
+     * @return string
+     */
+    public function getStubPath(): string;
 }
