c0954d95a1af58579df23a303dbe3cc627bb4a38
Jascha030 <contact@jaschavanaalst.nl>
2022-06-22 00:08:59 +0200

    🚧 Start ParserAbstract.

--- src/Config/Parser/ParserAbstract.php
+++ src/Config/Parser/ParserAbstract.php
@@ -1,8 +1,20 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Jascha030\Dotfiles\Config\Parser;
 
-class ParserAbstract
-{
+use Jascha030\Dotfiles\Config\ConfigInterface;
+use SplFileInfo;
 
+abstract class ParserAbstract implements ConfigFileParserInterface
+{
+    abstract public function getFilePattern(): array;
+
+    abstract public function parse(SplFileInfo $fileInfo): ConfigInterface;
+
+    private function getHome(): string
+    {
+        return getenv('HOME');
+    }
 }
