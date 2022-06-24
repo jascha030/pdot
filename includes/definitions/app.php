be9d3abd857847e8ccf67218e3ff55b93e12f60a
Jascha030 <contact@jaschavanaalst.nl>
2022-06-24 14:55:56 +0200

    🔧 Setup autowiring for Application class.

--- includes/definitions/app.php
+++ includes/definitions/app.php
@@ -5,9 +5,16 @@ declare(strict_types=1);
 use Jascha030\Dotfiles\Application;
 use Jascha030\Dotfiles\Console\Command\ConfigCommand;
 use Jascha030\Dotfiles\Console\Command\SyncCommand;
+use Symfony\Component\Console\Application as BaseApplication;
+use function DI\autowire;
 
+/**
+ * Console application definitions.
+ *
+ * @see https://php-di.org/doc/php-definitions.html
+ */
 return [
-    ConfigCommand::class,
-    SyncCommand::class,
-    Application::class,
+    ConfigCommand::class   => autowire(),
+    SyncCommand::class     => autowire(),
+    BaseApplication::class => autowire(Application::class),
 ];
