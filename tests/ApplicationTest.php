5a2948e56ecf3c0dd43baa06d281732ea089b253
Jascha030 <contact@jaschavanaalst.nl>
2022-06-24 14:22:42 +0200

    ✅ Add application test.

--- tests/ApplicationTest.php
+++ tests/ApplicationTest.php
@@ -1,10 +1,28 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Jascha030\Dotfiles;
 
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Application as BaseApplication;
+use function PHPUnit\Framework\assertEquals;
+use function PHPUnit\Framework\assertInstanceOf;
 
-class ApplicationTest extends TestCase
+/**
+ * @covers \Jascha030\Dotfiles\Application
+ *
+ * @internal
+ */
+final class ApplicationTest extends TestCase
 {
+    public function testConstruct(): void
+    {
+        $app = new Application();
 
+        assertInstanceOf(BaseApplication::class, $app);
+
+        assertEquals(Application::APP_NAME, $app->getName());
+        assertEquals(Application::VERSION, $app->getVersion());
+    }
 }
