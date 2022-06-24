e6562cbd151f4622c95b2c5873c1a545793f66bb
Jascha030 <contact@jaschavanaalst.nl>
2022-06-24 14:56:12 +0200

    ✅ Assert autowiring is working.

--- tests/ApplicationTest.php
+++ tests/ApplicationTest.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Jascha030\Dotfiles;
 
 use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 use Symfony\Component\Console\Application as BaseApplication;
 use function PHPUnit\Framework\assertEquals;
 use function PHPUnit\Framework\assertInstanceOf;
@@ -24,5 +26,17 @@ final class ApplicationTest extends TestCase
 
         assertEquals(Application::APP_NAME, $app->getName());
         assertEquals(Application::VERSION, $app->getVersion());
+    }
+
+    /**
+     * @depends testConstruct
+     *
+     * @throws ContainerExceptionInterface|NotFoundExceptionInterface
+     */
+    public function testContainerDefinition(): void
+    {
+        $app = container()->get(BaseApplication::class);
+
+        assertInstanceOf(Application::class, $app);
     }
 }
