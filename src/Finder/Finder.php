4b84ab63161b952f32f2ea93d520c22e52e4c89b
Jascha030 <contact@jaschavanaalst.nl>
2022-06-24 19:42:38 +0200

    ✨ implement factory methods for the two finder usecases.

--- src/Finder/Finder.php
+++ src/Finder/Finder.php
@@ -8,7 +8,20 @@ use Jascha030\Dotfiles\Config\ConfigInterface;
 
 class Finder extends \Symfony\Component\Finder\Finder
 {
-    public function setConfig(ConfigInterface $config): static
+    public static function dotfileFinder(ConfigInterface $config): static
+    {
+        return static::create()->setConfig($config);
+    }
+
+    /**
+     * @todo: implement method to setup for finding RC Files.
+     */
+    public static function configFinder(): static
+    {
+        return static::create();
+    }
+
+    private function setConfig(ConfigInterface $config): static
     {
         return $this
             ->ignoreDotFiles(false)
