3f2fb20562b76383f1e83f4a74b1baa3f843a18a
Jascha030 <contact@jaschavanaalst.nl>
2022-06-25 17:33:17 +0200

    ✨ ConfigFileRepository abstract.

--- src/Config/Repository/File/ConfigFileRepository.php
+++ src/Config/Repository/File/ConfigFileRepository.php
@@ -1,8 +1,41 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Jascha030\Dotfiles\Config\Repository\File;
 
-class ConfigFileParser
-{
+use ArrayIterator;
+use Iterator;
+use Jascha030\Dotfiles\Config\ConfigInterface;
+use Jascha030\Dotfiles\Config\Repository\ConfigRepository;
+use Symfony\Component\Finder\SplFileInfo;
 
+abstract class ConfigFileRepository extends ConfigRepository implements ConfigFileRepositoryInterface
+{
+    /**
+     * {@inheritDoc}
+     *
+     * @return null|ArrayIterator<string, ConfigInterface>
+     */
+    public function resolve(): null|Iterator
+    {
+        $results  = [];
+        $iterator = $this->getFinder()->getIterator();
+
+        /**
+         * @var SplFileInfo $file
+         */
+        foreach ($iterator as $file) {
+            $parsed = $this->getParser()->parse($file->getFileInfo()->getRealPath());
+            if (! $parsed) {
+                continue;
+            }
+
+            $results[$file->getRealPath()] = $parsed;
+        }
+
+        return ! empty($results)
+            ? new ArrayIterator($results)
+            : null;
+    }
 }
