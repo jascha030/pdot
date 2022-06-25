18dcd1abbda1758f5d5922d7e4af915907a4f83a
Jascha030 <contact@jaschavanaalst.nl>
2022-06-25 17:03:59 +0200

    ✅ Add unit tests for EnvironmentRepository.

--- tests/Config/Repository/EnvironmentRepositoryTest.php
+++ tests/Config/Repository/EnvironmentRepositoryTest.php
@@ -1,10 +1,70 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Jascha030\Dotfiles\Config\Repository;
 
 use PHPUnit\Framework\TestCase;
+use function PHPUnit\Framework\assertEquals;
+use function PHPUnit\Framework\assertIsString;
 
+/**
+ * @covers \Jascha030\Dotfiles\Config\Repository\ConfigRepository
+ * @covers \Jascha030\Dotfiles\Config\Repository\EnvironmentRepository
+ *
+ * @internal
+ */
 class EnvironmentRepositoryTest extends TestCase
 {
+    private const MAP = [
+        'PDOT_DESTINATION'    => 'getDestination',
+        'PDOT_DOTFILE_DIRS'   => 'getDotDirs',
+        'PDOT_UNDOT_PATTERNS' => 'getUndottedPatterns',
+    ];
 
+    private static ?array $testEnv = null;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$testEnv = [
+            'PDOT_DESTINATION'    => dirname(__FILE__, 3) . '/tests/Fixtures/fs/root',
+            'PDOT_DOTFILE_DIRS'   => dirname(__FILE__, 3) . '/tests/Fixtures/fs/root/.dotfiles',
+            'PDOT_UNDOT_PATTERNS' => ['undotmepls'],
+        ];
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        self::$testEnv = null;
+    }
+
+    public function testGetDescription(): void
+    {
+        assertIsString($this->getRepository()->getDescription());
+    }
+
+    public function testGetName(): void
+    {
+        assertIsString($this->getRepository()->getName());
+    }
+
+    public function testGetPriority(): void
+    {
+        assertEquals(ConfigRepository::PRIO_HIGH, $this->getRepository()->getPriority());
+    }
+
+    public function testResolve(): void
+    {
+        $_SERVER = array_merge($_SERVER, self::$testEnv);
+        $config  = $this->getRepository()->resolve();
+
+        foreach (self::$testEnv as $key => $value) {
+            assertEquals($value, $config->{self::MAP[$key]}());
+        }
+    }
+
+    private function getRepository(): ConfigRepositoryInterface
+    {
+        return new EnvironmentRepository();
+    }
 }
