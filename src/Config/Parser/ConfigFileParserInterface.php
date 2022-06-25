daa22cb24e14ed9ee0973965ddfb8a5e41656375
Jascha030 <contact@jaschavanaalst.nl>
2022-06-25 17:30:21 +0200

    ♻️ Change ConfigFileParserInterface::parse param to filepath string for flexibility.

--- src/Config/Parser/ConfigFileParserInterface.php
+++ src/Config/Parser/ConfigFileParserInterface.php
@@ -5,11 +5,8 @@ declare(strict_types=1);
 namespace Jascha030\Dotfiles\Config\Parser;
 
 use Jascha030\Dotfiles\Config\ConfigInterface;
-use SplFileInfo;
 
 interface ConfigFileParserInterface
 {
-    public function getFilePattern(): array;
-
-    public function parse(SplFileInfo $fileInfo): ConfigInterface;
+    public function parse(string $path): ?ConfigInterface;
 }
