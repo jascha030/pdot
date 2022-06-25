8f1367e43f1f77d4fac91138499cbb8402c31672
Jascha030 <contact@jaschavanaalst.nl>
2022-06-25 16:08:08 +0200

    🔧 Add definitions file with config defaults.

--- includes/definitions/defaults.php
+++ includes/definitions/defaults.php
@@ -1,1 +1,15 @@
 <?php
+
+declare(strict_types=1);
+
+return [
+    'default.home'            => $_SERVER['HOME'],
+    'default.dotdir'          => '.dotfiles',
+    'default.patterns.ignore' => [
+        '*.git*',
+        '*README*',
+        '*LICENSE*',
+    ],
+    'default.patterns.undot' => [
+    ],
+];
