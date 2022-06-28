bc72f00d84cc3ac3a9af8693a5bdddfecf9dec16
Jascha030 <contact@jaschavanaalst.nl>
2022-06-28 03:15:41 +0200

    🚧 Start native file parser repository for simple php config files like in e.g. php-cs-fixer.

--- src/Config/Repository/File/NativeFileRepository.php
+++ src/Config/Repository/File/NativeFileRepository.php
@@ -1,8 +1,29 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Jascha030\Dotfiles\Config\Repository\File;
 
-class NativeFileRepository
+use Jascha030\Dotfiles\Config\Parser\ConfigFileParserInterface;
+use Jascha030\Dotfiles\Finder\Finder;
+
+class NativeFileRepository extends ConfigFileRepository
 {
 
+    /**
+     * {@inheritDoc}
+     */
+    public function getStubPath(): ?string
+    {
+        // TODO: Implement getStubPath() method.
+    }
+
+    public function getAllowedPatterns(): array
+    {
+        return [
+            '.pdot.*.php',
+            '.pdot.php',
+            
+        ];
+    }
 }
