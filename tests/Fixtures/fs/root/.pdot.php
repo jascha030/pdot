5c7245616a29d85350985985b01bf912a001a998
Jascha030 <contact@jaschavanaalst.nl>
2022-06-29 01:24:28 +0200

    💡 Add doc.

--- tests/Fixtures/fs/root/.pdot.php
+++ tests/Fixtures/fs/root/.pdot.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 use Jascha030\Dotfiles\Config\Config;
 
+/**
+ * Config file used by PHPUnit.
+ *
+ * @return Config
+ */
 return Config::create()
     ->setDotDirs(__DIR__ . '/.dotfiles')
     ->setDestination(__DIR__)
