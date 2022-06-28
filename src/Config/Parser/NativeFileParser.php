85db650ea0ae94ddc033d0525aacd7d2ad98bd75
Jascha030 <contact@jaschavanaalst.nl>
2022-06-28 03:17:43 +0200

    ✨ Implement most simple parser (doesnt realy have to parse much).

--- src/Config/Parser/NativeFileParser.php
+++ src/Config/Parser/NativeFileParser.php
@@ -1,8 +1,26 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Jascha030\Dotfiles\Config\Parser;
 
-class NativeFileParser
-{
+use Jascha030\Dotfiles\Config\ConfigInterface;
+use Jascha030\Dotfiles\Exception\Config\Parser\ParserException;
 
+final class NativeFileParser implements ConfigFileParserInterface
+{
+    public function parse(string $path): ConfigInterface
+    {
+        if (! file_exists($path)) {
+            throw new ParserException($path);
+        }
+
+        $contents = include $path;
+
+        if (! is_subclass_of($contents, ConfigInterface::class)) {
+            throw new ParserException($path);
+        }
+
+        return $contents;
+    }
 }
