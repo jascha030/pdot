ff4a4add7083dbfeee0448a10e7d33c6dedde7d8
Jascha030 <contact@jaschavanaalst.nl>
2022-06-29 01:21:52 +0200

    🍱 Test config file.

--- tests/Fixtures/fs/root/.pdot.php
+++ tests/Fixtures/fs/root/.pdot.php
@@ -1,1 +1,11 @@
 <?php
+
+declare(strict_types=1);
+
+use Jascha030\Dotfiles\Config\Config;
+
+return Config::create()
+    ->setDotDirs(__DIR__ . '/.dotfiles')
+    ->setDestination(__DIR__)
+    ->setUndottedPatterns(['undotmepls'])
+    ->setIgnoredPatterns(['ignoremepls']);
