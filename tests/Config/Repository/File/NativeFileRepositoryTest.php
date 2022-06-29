7467c4c201943b190966d16ee89836d97793d56d
Jascha030 <contact@jaschavanaalst.nl>
2022-06-29 02:17:20 +0200

    ✅ Add unit tests for NativeFileRepository.

---
+++ tests/Config/Repository/File/NativeFileRepositoryTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jascha030\Dotfiles\Config\Repository\File;
+
+use Jascha030\Dotfiles\Finder\Finder;
+use PHPUnit\Framework\TestCase;
+use function PHPUnit\Framework\assertCount;
+use function PHPUnit\Framework\assertInstanceOf;
+use function PHPUnit\Framework\assertNull;
+
+/**
+ * @covers \Jascha030\Dotfiles\Config\Repository\File\ConfigFileRepository
+ * @covers \Jascha030\Dotfiles\Config\Repository\File\NativeFileRepository
+ *
+ * @internal
+ */
+final class NativeFileRepositoryTest extends TestCase
+{
+    public function testConstruct(): void
+    {
+        assertInstanceOf(
+            ConfigFileRepositoryInterface::class,
+            (new NativeFileRepository())
+        );
+    }
+
+    /**
+     * @depends testConstruct
+     */
+    public function testGetAllowedPatterns(): void
+    {
+        $patterns = (new NativeFileRepository())->getAllowedPatterns();
+
+        $iterator = Finder::configFinder()
+            ->in(dirname(__FILE__, 4) . '/Fixtures/fs/root')
+            ->name($patterns)
+            ->getIterator();
+
+        assertCount(1, iterator_to_array($iterator));
+    }
+
+    /**
+     * @depends testConstruct
+     */
+    public function testGetStubPath(): void
+    {
+        assertNull((new NativeFileRepository())->getStubPath());
+    }
+}
