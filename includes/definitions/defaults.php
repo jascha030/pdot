f9c1e2bd80baa40b10d13ead7804aefae0ee6b5c
Jascha030 <contact@jaschavanaalst.nl>
2022-06-30 12:41:25 +0200

    🔧 Update defaults definitions to include default config.

--- includes/definitions/defaults.php
+++ includes/definitions/defaults.php
@@ -2,14 +2,14 @@
 
 declare(strict_types=1);
 
+use Jascha030\Dotfiles\Config\Config;
+use Psr\Container\ContainerInterface;
+
 return [
-    'default.home'            => $_SERVER['HOME'],
-    'default.dotdir'          => '.dotfiles',
-    'default.patterns.ignore' => [
-        '*.git*',
-        '*README*',
-        '*LICENSE*',
-    ],
-    'default.patterns.undot' => [
-    ],
+    'default.home'   => $_SERVER['HOME'],
+    'default.dotdir' => sprintf('%s/.dotfiles', $_SERVER['HOME']),
+    'default.config' => static fn (ContainerInterface $container): Config => Config::create()
+        ->setDestination($container->get('default.home'))
+        ->setDotDirs($container->get('default.dotdir'))
+        ->setOrigin('Default config.'),
 ];
