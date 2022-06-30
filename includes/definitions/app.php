0de30f145bd3db3b5b98cbf9ed99fd8c6fe9627f
Jascha030 <contact@jaschavanaalst.nl>
2022-06-30 12:22:53 +0200

    🔧 Add definition for NativeFileRepository and ConfigResolver.

--- includes/definitions/app.php
+++ includes/definitions/app.php
@@ -3,8 +3,11 @@
 declare(strict_types=1);
 
 use Jascha030\Dotfiles\Application;
+use Jascha030\Dotfiles\Config\ConfigResolver;
+use Jascha030\Dotfiles\Config\Repository\File\NativeFileRepository;
 use Jascha030\Dotfiles\Console\Command\ConfigCommand;
 use Jascha030\Dotfiles\Console\Command\UpCommand;
+use Psr\Container\ContainerInterface;
 use Symfony\Component\Console\Application as BaseApplication;
 use function DI\autowire;
 
@@ -14,7 +17,19 @@ use function DI\autowire;
  * @see https://php-di.org/doc/php-definitions.html
  */
 return [
+    'repositories' => [
+        NativeFileRepository::class,
+    ],
+    ConfigResolver::class => static function (ContainerInterface $container): ConfigResolver {
+        return new ConfigResolver(iterator_to_array(
+            (static function (array $repositories) use ($container) {
+                foreach ($repositories as $class) {
+                    yield $container->get($class);
+                }
+            })($container->get('repositories'))
+        ));
+    },
     ConfigCommand::class   => autowire(),
-    UpCommand::class       => autowire(),
+    UpCommand::class       => autowire()->constructor(),
     BaseApplication::class => autowire(Application::class),
 ];
