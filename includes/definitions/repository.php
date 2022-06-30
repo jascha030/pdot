034f756ba29676f36a025ecb9f298e248a3a355b
Jascha030 <contact@jaschavanaalst.nl>
2022-06-30 12:41:50 +0200

    🔧 Add definitions file for ConfigRepositories.

--- includes/definitions/repository.php
+++ includes/definitions/repository.php
@@ -1,1 +1,20 @@
 <?php
+
+declare(strict_types=1);
+
+use Jascha030\Dotfiles\Config\Parser\NativeFileParser;
+use Jascha030\Dotfiles\Config\Repository\File\NativeFileRepository;
+use function DI\autowire;
+use function DI\create;
+use function DI\get;
+
+return [
+    NativeFileParser::class     => autowire(),
+    NativeFileRepository::class => create(NativeFileRepository::class)->method(
+        'setParser',
+        get(NativeFileRepository::class)
+    ),
+    'repositories' => [
+        NativeFileRepository::class,
+    ],
+];
