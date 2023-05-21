2f91607f713b560abf18c69c4e50442e423fbd8f
Jascha030 <contact@jaschavanaalst.nl>
2023-05-22 01:18:05 +0200

    🔒️ Somewhat secure include

--- src/Config/Parser/NativeFileParser.php
+++ src/Config/Parser/NativeFileParser.php
@@ -6,21 +6,20 @@ namespace Jascha030\Dotfiles\Config\Parser;
 
 use Jascha030\Dotfiles\Config\ConfigInterface;
 use Jascha030\Dotfiles\Exception\Config\Parser\ParserException;
+use TypeError;
 
 final class NativeFileParser implements ConfigFileParserInterface
 {
     public function parse(string $path): ConfigInterface
     {
-        if (! file_exists($path)) {
+        if (!file_exists($path)) {
             throw new ParserException($path);
         }
 
-        $contents = include $path;
-
-        if (! is_subclass_of($contents, ConfigInterface::class)) {
-            throw new ParserException($path);
+        try {
+            return (static fn (): ConfigInterface => include $path)();
+        } catch (TypeError) {
+            throw new ParserException($path, ParserException::REASON_INVALID_CONFIG);
         }
-
-        return $contents;
     }
 }
